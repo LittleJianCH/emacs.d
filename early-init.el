a6022648cbe2e1278aa9009d75aca67d452b3725
LittleJian <2447746158@qq.com>
2022-01-16 01:18:26 +0800

    use straight as package manager

---
+++ early-init.el
@@ -0,0 +1,7 @@
+(add-to-list 'load-path (expand-file-name "module" user-emacs-directory))
+
+(setq url-proxy-services
+   '(("http" . "127.0.0.1:7890")
+     ("https" . "127.0.0.1:7890")))
+
+(require 'init-straight)
